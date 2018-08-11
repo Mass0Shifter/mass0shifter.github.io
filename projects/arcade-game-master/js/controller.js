ba21c51c5c12ca0c95c9c4123c3d3e6fc094c2e9
Mass0Shifter <41183530+Mass0Shifter@users.noreply.github.com>
2018-08-11 21:58:55 +0100

    Update controller.js

--- projects/arcade-game-master/js/controller.js
+++ projects/arcade-game-master/js/controller.js
@@ -1,12 +1,19 @@
 // Game onscreen Controller
 
-$("document").load(addListener);
+$("document").load(function(){
+ addListener();
+ enlargeButtons();
+});
+
+function enlargeButtons(){
+  $(".movement").css(["width", "height"], "20%");
+}
 
 function addListeners(){
- $("#moveleft").on("click", {direction:"left"}, move);
- $("#moveright").on("click", {direction:"right"}, move);
- $("#moveup").on("click", {direction:"up"}, move);
- $("#movedown").on("click", {direction:"down"}, move);
+  $("#moveleft").on("click", {direction:"left"}, move);
+  $("#moveright").on("click", {direction:"right"}, move);
+  $("#moveup").on("click", {direction:"up"}, move);
+  $("#movedown").on("click", {direction:"down"}, move);
 }
 
 function move(event){
